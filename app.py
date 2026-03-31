3f9cd9d49313edab4654cf10b6adaf05f5e34e79
sudipta modak <babu7sudipta@gmail.com>
2026-03-31 17:33:45 +0530

    subtraction updated

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def addition(a, b):
     return a + b
 
-def subtraction(a, b):
-    return a - b
+def subtraction(a, b, c):
+    return a - b - c
 
 def division(a,b):
     return "Error: Division by zero is not allowed" if b == 0 else a / b
